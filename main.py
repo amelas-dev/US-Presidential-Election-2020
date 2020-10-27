ec2a793b1392a32718077e572b54b2450a7b68c8
Alex Melas <35109530+amelas-dev@users.noreply.github.com>
2020-10-27 14:10:34 -0400

    Edit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Imports
+
+#Files will be split between a graphics file, data file, and a main file to run program
+
+# Classes
+
+#   GUI Class
+#   Between either Tkinter or a web framework like flask or Django
+
+#   Canidate Class
+
+#   State Class
+
+#Main to run/dispay 
+
