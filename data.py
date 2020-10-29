afa78fc77e027ad9d75d9cc43c6aec1a1fd2b240
Alex Melas <35109530+amelas-dev@users.noreply.github.com>
2020-10-29 13:06:48 -0400

    reader

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # All the state and canidate classes go here
+from csv import reader
+
+with open('presidential_data.csv','r') as read_obj:
+    csv_reader = reader(read_obj)
+    canidateData = list(csv_reader)
+canidateDataKeys = canidateData[0]
+canidateDataNumbers = canidateData[1:]
